2a6b918f32f9b259e3680546ec7855c3b4d68675
Lucía <luciaft2000@hotmail.com>
2024-09-06 17:55:49 +0200

    pictures carrousel

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
+const walkrosImages = [
+    'images/Walkros/redEye.png',
+    'images/Walkros/walkros-1.png',
+    'images/Walkros/walkros-2.jpg',
+    'images/Walkros/greenEye.png', 
+    'images/Walkros/lobo.png',
+    'images/Walkros/Josua.png',
+];
+
+
 
 function CreateAlert(text){
     alert(text)
 }
+
+function createSlideshow(imageArray, elementId, interval) {
+    let currentIndex = 0;
+    const slideshowElement = document.getElementById(elementId);
+
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageArray.length;
+        slideshowElement.src = imageArray[currentIndex];
+    }
+
+    setInterval(changeImage, interval);
+}
+createSlideshow(walkrosImages, 'slideshow-image-walkros', 2000);
+createSlideshow(anotherSlideshowImages, 'slideshow-image-another', 3000);
